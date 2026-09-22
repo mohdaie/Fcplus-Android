ff47c87a47c5c980e68c647a62ee404b426328bb
mohdaie <ayiepts@gmail.com>
2026-09-22 10:52:47 +0800

    Align Android and Kotlin JVM target to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,11 @@ android {
         versionName = "0.1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -42,4 +47,9 @@ dependencies {
     implementation("androidx.webkit:webkit:1.12.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+}
+
+
+kotlin {
+    jvmToolchain(17)
 }
