42e0cbeab43d7a5c1c17444d79a11c225dc01841
mohdaie <ayiepts@gmail.com>
2026-09-22 11:33:05 +0800

    Use desktop-compatible EA Web App viewport

--- app/src/main/java/com/fcplus/android/WebViewTools.kt
+++ app/src/main/java/com/fcplus/android/WebViewTools.kt
@@ -8,6 +8,10 @@ import android.webkit.WebView
 object WebViewTools {
     const val EA_URL = "https://www.ea.com/ea-sports-fc/ultimate-team/web-app/"
 
+    private const val DESKTOP_UA =
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " +
+        "(KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+
     fun configure(webView: WebView) {
         with(webView.settings) {
             javaScriptEnabled = true
@@ -15,9 +19,23 @@ object WebViewTools {
             databaseEnabled = true
             cacheMode = WebSettings.LOAD_DEFAULT
             mediaPlaybackRequiresUserGesture = true
+
+            // EA's Web App rejects some narrow/mobile WebView viewports with the
+            // misleading "Rotate Device" screen. Keep Android orientation natural,
+            // but present a desktop-class browser viewport like mobile browsers'
+            // "Desktop site" mode.
+            userAgentString = DESKTOP_UA
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            setSupportZoom(true)
+            builtInZoomControls = true
+            displayZoomControls = false
+
             allowFileAccess = false
             allowContentAccess = false
         }
+
+        webView.setInitialScale(0)
 
         CookieManager.getInstance().apply {
             setAcceptCookie(true)
