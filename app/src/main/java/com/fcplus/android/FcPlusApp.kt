fda326016e722ddddf783fa9bc38442c7f8180cd
mohdaie <ayiepts@gmail.com>
2026-09-22 11:55:39 +0800

    Restore shared Android app context

--- app/src/main/java/com/fcplus/android/FcPlusApp.kt
+++ app/src/main/java/com/fcplus/android/FcPlusApp.kt
@@ -1,10 +1,15 @@
 package com.fcplus.android
 
 import android.app.Application
+import android.content.Context
 
 class FcPlusApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AppContextHolder.context = applicationContext
     }
+}
+
+object AppContextHolder {
+    lateinit var context: Context
 }
