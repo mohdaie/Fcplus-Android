02692f8aa82b08559990cf1e1585156917445edb
mohdaie <ayiepts@gmail.com>
2026-09-22 13:59:04 +0800

    Bump FC+ to v0.4.3 build 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fcplus.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.4.2"
+        versionCode = 12
+        versionName = "0.4.3"
 
         ndk {
             abiFilters += "arm64-v8a"
